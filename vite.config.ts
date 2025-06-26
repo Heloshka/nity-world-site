453c13fb0613d3f12daf02599509418be827e3ff
Heloshka <ygh9590@gmail.com>
2025-06-27 01:53:48 +0300

    fix: remove base path for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/nity-world-site/', // 👈 ОБОВʼЯЗКОВО для GitHub Pages
   plugins: [react()],
 })
